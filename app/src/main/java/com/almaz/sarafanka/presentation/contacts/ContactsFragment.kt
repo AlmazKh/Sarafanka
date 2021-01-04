65b3db3779bb5cbc2e83424a558c54eee42a12b9
AlmazKh <hamedzhanovalmaz@gmail.com>
2021-01-04 22:27:31 +0300

    [Fix] fixed issue with permission result

--- app/src/main/java/com/almaz/sarafanka/presentation/contacts/ContactsFragment.kt
+++ app/src/main/java/com/almaz/sarafanka/presentation/contacts/ContactsFragment.kt
@@ -5,35 +5,63 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.core.os.bundleOf
 import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.Observer
 import com.almaz.sarafanka.R
 import com.almaz.sarafanka.core.model.Contact
 import com.almaz.sarafanka.presentation.base.BaseFragment
+import com.almaz.sarafanka.utils.extensions.observe
+import com.almaz.sarafanka.utils.extensions.toVisible
+import com.faltenreich.skeletonlayout.applySkeleton
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_contacts.*
 
+private const val CONTACTS_READ_REQ_CODE = 100
 
 class ContactsFragment : BaseFragment<ContactsViewModel>(ContactsViewModel::class.java) {
     override val layoutId = R.layout.fragment_contacts
-    private val CONTACTS_SMS_CODE = 101
-    private val CONTACTS_READ_REQ_CODE = 100
-
+    lateinit var adapter: ContactsAdapter
 
     override fun setupView() {
-        tvDefault.text = "Fetching contacts..."
-        val adapter = ContactsAdapter(rootActivity, ::onContactClick)
-        rvContacts.adapter = adapter
-        viewModel.contactsLiveData.observe(this, Observer {
-            tvDefault.visibility = View.GONE
+//        viewModel.loadingState.loading()
+//        tvDefault.text = "Загрузка контактов..."
+        adapter = ContactsAdapter(rootActivity, ::onContactClick)
+        rv_contacts.adapter = adapter
+        /*viewModel.contactsLiveData.observe(viewLifecycleOwner, Observer {
+//            viewModel.loadingState.ready()
+//            tvDefault.visibility = View.GONE
             adapter.submitGlobalList(it)
-        })
+        })*/
+
+        skeletons.add(rv_contacts.applySkeleton(R.layout.row_contact, 6))
 
         et_search_contacts.addTextChangedListener {
             adapter.filter.filter(it)
         }
+        fetchContacts()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        rootActivity.bottom_nav.toVisible()
+    }
+
+    override fun subscribe(viewModel: ContactsViewModel) {
+        super.subscribe(viewModel)
+        observe(viewModel.contactsLiveData, ::bindContacts)
+    }
+
+    private fun bindContacts(list: List<Contact>) {
+        adapter.submitGlobalList(list)
+    }
+
+//    override fun onRefresh() {
+//        super.onRefresh()
+//        fetchContacts()
+//    }
+
+    private fun fetchContacts() {
         if (ActivityCompat.checkSelfPermission(
                 rootActivity,
                 Manifest.permission.READ_CONTACTS
@@ -51,8 +79,7 @@ class ContactsFragment : BaseFragment<ContactsViewModel>(ContactsViewModel::clas
                     setTitle("Permission")
                     setMessage("Need contacts read permission for fetching and displaying contacts")
                     setPositiveButton("Ok") { _, _ ->
-                        ActivityCompat.requestPermissions(
-                            rootActivity,
+                        requestPermissions(
                             arrayOf(Manifest.permission.READ_CONTACTS),
                             CONTACTS_READ_REQ_CODE
                         )
@@ -61,15 +88,13 @@ class ContactsFragment : BaseFragment<ContactsViewModel>(ContactsViewModel::clas
                     show()
                 }
             } else {
-                ActivityCompat.requestPermissions(
-                    rootActivity,
+                requestPermissions(
                     arrayOf(Manifest.permission.READ_CONTACTS),
                     CONTACTS_READ_REQ_CODE
                 )
             }
         }
     }
-
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -92,7 +117,12 @@ class ContactsFragment : BaseFragment<ContactsViewModel>(ContactsViewModel::clas
         if (contact.user == null) {
             val uri = Uri.parse("smsto:${contact.numbers.last()}")
             val intent = Intent(Intent.ACTION_SENDTO, uri)
-            intent.putExtra("sms_body", "Скачивай сарафанку а то че как лох")
+            intent.putExtra(
+                "sms_body", "Привет! Не знаешь как заработать на своих навыках?" +
+                        "Названиешь каждому знакомому с просьбой помочь? Хватит! " +
+                        "Скорее скачивай приложение \"Sarafanka\"!\n\n" +
+                        "Переходи в Play Store: (будет ссылка)"
+            )
             startActivity(intent)
         }
 /*
